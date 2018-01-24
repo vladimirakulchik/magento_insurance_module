21aa23779819cb127168409ee33b83f3ab035f09
Vladimir Akulchik <vladimirakulchyk@gmail.com>
2018-01-24 17:39:17 +0300

    Update template for all shipping methods

--- Insurance/app/code/local/Itransition/Insurance/Block/Checkbox.php
+++ Insurance/app/code/local/Itransition/Insurance/Block/Checkbox.php
@@ -5,7 +5,17 @@ class Itransition_Insurance_Block_Checkbox extends Mage_Core_Block_Template
     public function isModuleEnable()
     {
         $moduleEnabled = Mage::getStoreConfigFlag('insurance/settings/enabled');
-        var_dump($moduleEnabled);
         return $moduleEnabled;
+    }
+
+    public function getShippingMethods()
+    {
+        $methods = Mage::getSingleton('shipping/config')->getActiveCarriers();
+        $shipMethods = array();
+        foreach ($methods as $shippingCode => $shippingModel)
+        {
+            $shipMethods[] = $shippingCode;
+        }
+        return $shipMethods;
     }
 }
