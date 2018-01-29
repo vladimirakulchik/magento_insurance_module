b1416cbd8dcebdc6f4c8d9803be7504b982c4331
Vladimir Akulchik <vladimirakulchyk@gmail.com>
2018-01-29 15:29:30 +0300

    Add information about insurance for admin

---
+++ Insurance/app/code/local/Itransition/Insurance/Block/Adminhtml/Order/Invoice/Totals.php
@@ -0,0 +1,13 @@
+<?php
+
+class Itransition_Insurance_Block_Adminhtml_Order_Invoice_Totals extends Mage_Sales_Block_Order_Invoice_Totals
+{
+    protected $_code = 'insurance';
+
+    protected function _initTotals()
+    {
+        parent::_initTotals();
+        Mage::helper('insurance')->addInsuranceToTotals($this);
+        return $this;
+    }
+}
---
+++ Insurance/app/code/local/Itransition/Insurance/Block/Adminhtml/Order/Totals.php
@@ -0,0 +1,13 @@
+<?php
+
+class Itransition_Insurance_Block_Adminhtml_Order_Totals extends Mage_Sales_Block_Order_Totals
+{
+    protected $_code = 'insurance';
+
+    protected function _initTotals()
+    {
+        parent::_initTotals();
+        Mage::helper('insurance')->addInsuranceToTotals($this);
+        return $this;
+    }
+}
